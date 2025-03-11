4c3fc9ae491810b5649cd08e265e94908701ee2e
kkn1125 <xpfjrnseks1@naver.com>
2025-03-11 10:42:35 +0900

    ✨ add: guard 적용 + swagger summary 추가

--- src/restaurants/restaurants.controller.ts
+++ src/restaurants/restaurants.controller.ts
@@ -6,30 +6,42 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { RestaurantsService } from './restaurants.service';
 import { CreateRestaurantDto } from './dto/create-restaurant.dto';
 import { UpdateRestaurantDto } from './dto/update-restaurant.dto';
+import { RoleGuard } from '@auth/guard/role.guard';
+import { Roles } from '@auth/guard/roles.decorator';
+import { UserRole } from '@users/enums/UserRole';
+import { ApiOperation } from '@nestjs/swagger';
 
+@UseGuards(RoleGuard)
 @Controller('restaurants')
 export class RestaurantsController {
   constructor(private readonly restaurantsService: RestaurantsService) {}
 
+  @ApiOperation({ summary: '식당 정보 추가' })
+  @Roles([UserRole.Shopkeeper])
   @Post()
   create(@Body() createRestaurantDto: CreateRestaurantDto) {
     return this.restaurantsService.create(createRestaurantDto);
   }
 
+  @ApiOperation({ summary: '식당 전체 조회' })
   @Get()
   findAll() {
     return this.restaurantsService.findAll();
   }
 
+  @ApiOperation({ summary: '식당 상세 조회' })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.restaurantsService.findOne(+id);
   }
 
+  @ApiOperation({ summary: '식당 정보 수정' })
+  @Roles([UserRole.Shopkeeper])
   @Patch(':id')
   update(
     @Param('id') id: string,
@@ -38,6 +50,8 @@ export class RestaurantsController {
     return this.restaurantsService.update(+id, updateRestaurantDto);
   }
 
+  @ApiOperation({ summary: '식당 삭제' })
+  @Roles([UserRole.Shopkeeper])
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.restaurantsService.remove(+id);
