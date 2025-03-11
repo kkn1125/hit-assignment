d20092b51cc5f8f011d7392d2e166513022dc71c
kkn1125 <xpfjrnseks1@naver.com>
2025-03-11 10:40:57 +0900

    🛠️ fix: UserRole 타입 지정

--- src/auth/guard/roles.decorator.ts
+++ src/auth/guard/roles.decorator.ts
@@ -1,3 +1,4 @@
 import { SetMetadata } from '@nestjs/common';
+import { UserRole } from '@users/enums/UserRole';
 
-export const Roles = (args: string[] = []) => SetMetadata('roles', args);
+export const Roles = (args: UserRole[] = []) => SetMetadata('roles', args);
