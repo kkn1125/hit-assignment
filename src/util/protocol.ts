c3c235c2a7f1249414f06c699509f76d096fcd67
kkn1125 <xpfjrnseks1@naver.com>
2025-03-11 15:11:28 +0900

    ✨ add: 프로토콜 추가, 정리

--- src/util/protocol.ts
+++ src/util/protocol.ts
@@ -7,7 +7,17 @@ export const Protocol = {
   Conflict: { errorCode: 1004, message: '리소스가 중복됩니다.' }, // 409
   ServerError: { errorCode: 1005, message: '서버에 문제가 발생했습니다.' }, // 500
 
-  WrongCookieType: { errorCode: 2000, message: '잘못된 쿠키 정보입니다.' }, // 400
+  /* auth error */
+  RequiredLogin: { errorCode: 2000, message: '로그인이 필요합니다.' }, // 401
+  NoMatchRoles: { errorCode: 2001, message: '사용자 권한이 없습니다.' }, // 401
+  CookieWrongType: { errorCode: 2002, message: '잘못된 쿠키 정보입니다.' }, // 400
+  JwtWrongSignature: { errorCode: 2003, message: '잘못된 서명입니다.' }, // 401
+  JwtExpired: { errorCode: 2004, message: '토큰이 만료되었습니다.' }, // 401
+  JwtMalFormed: { errorCode: 2005, message: '잘못된 토큰 형태입니다.' }, // 401
+  JwtServerException: {
+    errorCode: 2006,
+    message: '토큰 인증에 문제가 발생했습니다.',
+  }, // 401
 } as const;
 export type Protocol = (typeof Protocol)[keyof typeof Protocol];
 
