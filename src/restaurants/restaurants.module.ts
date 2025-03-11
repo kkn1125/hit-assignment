223bac3c8cea89cc382d3a79483e5460de4f70ef
kkn1125 <xpfjrnseks1@naver.com>
2025-03-11 15:59:21 +0900

    🛠️ fix: import 정리

--- src/restaurants/restaurants.module.ts
+++ src/restaurants/restaurants.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { RestaurantsService } from './restaurants.service';
-import { RestaurantsController } from './restaurants.controller';
+import { RouterModule } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Restaurant } from './entities/restaurant.entity';
 import { MenusModule } from './menus/menus.module';
 import { ReservationsModule } from './reservations/reservations.module';
-import { RouterModule } from '@nestjs/core';
+import { RestaurantsController } from './restaurants.controller';
+import { RestaurantsService } from './restaurants.service';
 
 @Module({
   imports: [
