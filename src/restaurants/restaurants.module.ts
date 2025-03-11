3fd4abe9d05073f2b1e440f336f2e7a71fa69aea
kkn1125 <xpfjrnseks1@naver.com>
2025-03-11 10:11:21 +0900

    ✨ feat: 메뉴, 예약 도메인 식당 하위로 추가

--- src/restaurants/restaurants.module.ts
+++ src/restaurants/restaurants.module.ts
@@ -3,9 +3,20 @@ import { RestaurantsService } from './restaurants.service';
 import { RestaurantsController } from './restaurants.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Restaurant } from './entities/restaurant.entity';
+import { MenusModule } from './menus/menus.module';
+import { ReservationsModule } from './reservations/reservations.module';
+import { RouterModule } from '@nestjs/core';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Restaurant])],
+  imports: [
+    TypeOrmModule.forFeature([Restaurant]),
+    RouterModule.register([
+      { path: ':restaurantId', module: MenusModule },
+      { path: ':restaurantId', module: ReservationsModule },
+    ]),
+    MenusModule,
+    ReservationsModule,
+  ],
   controllers: [RestaurantsController],
   providers: [RestaurantsService],
 })
