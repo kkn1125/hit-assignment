c8d6efa0ae8482f8730696864ff50e68d314c1a4
kkn1125 <xpfjrnseks1@naver.com>
2025-03-11 17:04:13 +0900

    🛠️ fix: 식당 엔티티 상속

--- src/restaurants/dto/create-restaurant.dto.ts
+++ src/restaurants/dto/create-restaurant.dto.ts
@@ -1,1 +1,12 @@
-export class CreateRestaurantDto {}
+import { OmitType } from '@nestjs/swagger';
+import { Restaurant } from '@restaurants/entities/restaurant.entity';
+
+export class CreateRestaurantDto extends OmitType(Restaurant, [
+  'id',
+  'createdAt',
+  'updatedAt',
+  'deletedAt',
+  'user',
+  'reservations',
+  'menus',
+]) {}
