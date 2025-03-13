879fcdec85ad82988ca5349aee78357f8b97a964
kkn1125 <xpfjrnseks1@naver.com>
2025-03-13 17:49:18 +0900

    🛠️ fix: 식당 엔티티 추가

--- src/restaurants/reservations/reservations.module.ts
+++ src/restaurants/reservations/reservations.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Restaurant } from '@restaurants/entities/restaurant.entity';
 import { User } from '@users/entities/user.entity';
 import { Reservation } from './entities/reservation.entity';
 import { ReservationsController } from './reservations.controller';
 import { ReservationsService } from './reservations.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Reservation, User])],
+  imports: [TypeOrmModule.forFeature([Reservation, Restaurant, User])],
   controllers: [ReservationsController],
   providers: [ReservationsService],
 })
