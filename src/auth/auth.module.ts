bfb8976d937434fa40993a6adc99424953211a5c
kkn1125 <xpfjrnseks1@naver.com>
2025-03-13 16:54:33 +0900

    🛠️ fix: throwNoExists* 전역 함수 인자값 위해 user 도메인 repository, service 주입

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '@users/entities/user.entity';
+import { UsersService } from '@users/users.service';
 import { AuthController } from './auth.controller';
-import { UsersModule } from '@users/users.module';
+import { AuthService } from './auth.service';
 
 @Module({
-  imports: [UsersModule],
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, UsersService],
 })
 export class AuthModule {}
