c03d3c652e9061f0c7044866d6cc4af20b51740f
kkn1125 <xpfjrnseks1@naver.com>
2025-03-13 16:41:59 +0900

    🛠️ fix: omit type → pick type 변경 (명시적으로 표시하기 위함)

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,11 +1,11 @@
-import { OmitType } from '@nestjs/swagger';
+import { PickType } from '@nestjs/swagger';
 import { User } from '@users/entities/user.entity';
 
-export class CreateUserDto extends OmitType(User, [
-  'id',
-  'createdAt',
-  'updatedAt',
-  'deletedAt',
-  'restaurants',
-  'reservations',
+export class CreateUserDto extends PickType(User, [
+  'userId',
+  'phone',
+  'email',
+  'username',
+  'password',
+  'role',
 ]) {}
