f9f4abb96eddff54a5cde6d49fc1cff41df000d6
kkn1125 <xpfjrnseks1@naver.com>
2025-03-13 16:53:49 +0900

    ✨ feat: 식당 존재 여부 미들웨어에서 검증

---
+++ src/restaurants/middleware/restaurant-exists.middleware.ts
@@ -0,0 +1,25 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Restaurant } from '@restaurants/entities/restaurant.entity';
+import { throwNoExistsEntityWithSelectBy } from '@util/utilFunction';
+import { Request } from 'express';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class RestaurantExistsMiddleware implements NestMiddleware {
+  constructor(
+    @InjectRepository(Restaurant)
+    private readonly repository: Repository<Restaurant>,
+  ) {}
+
+  async use(req: Request, _res, next: () => void) {
+    console.log('🛠️🛠️🛠️✨✨✨here??');
+    const restaurantId = req.params.restaurantId;
+
+    await throwNoExistsEntityWithSelectBy(this.repository, {
+      id: +restaurantId,
+    });
+
+    next();
+  }
+}
