6f5c9fb9aeaadc4faf1ef55eddb517d64f0b467a
kkn1125 <xpfjrnseks1@naver.com>
2025-03-15 15:43:21 +0900

    🛠️ fix: 더미 데이터 및 sql export 준비

--- lib/1741439368275-migration.ts
+++ lib/1741439368275-migration.ts
@@ -11,7 +11,7 @@ export class Migration1741439368275 implements MigrationInterface {
     await queryRunner.query('ALTER TABLE `restaurant` AUTO_INCREMENT = 1');
     await queryRunner.query('ALTER TABLE `reservation` AUTO_INCREMENT = 1');
 
-    const users = createDummyUser(3);
+    const users = createDummyUser();
 
     await queryRunner.query('INSERT INTO user');
   }
