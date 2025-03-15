9f68e7ed2884ce6dc91f618c519c3514518aac9a
kkn1125 <xpfjrnseks1@naver.com>
2025-03-16 03:26:51 +0900

    🐛🛠️ bug&fix: 포트 지정 오류 수정

--- src/util/common.ts
+++ src/util/common.ts
@@ -2,7 +2,7 @@ import { PORT, RUN_MODE, VERSION } from '@common/variables/environment';
 
 export const commonOption = {
   version: VERSION,
-  port: RUN_MODE === 'development' ? PORT : 443,
+  port: PORT,
   allowOrigins:
     RUN_MODE === 'development'
       ? ['localhost:8080']
